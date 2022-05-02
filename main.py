d8a7125fec831b4d0e20e1b0717896ad1c058440
JRudransh <JRudransh@protonmail.com>
2022-05-02 15:40:00 +0530

    Day 1

---
+++ main.py
@@ -0,0 +1,66 @@
+"""
+This file can print greetings
+"""
+from pprint import pprint
+
+
+greet = 'Hello Python'
+_say_hii = 'Hii Python'
+hello2 = 'Hello world'
+
+# print(greet)
+# print(_say_hii)
+# print(hello2)
+
+
+# Datatype
+
+# string
+s1 = 'this is a string'
+s2 = "this is also a string"
+s3 = """This is also a string
+this is multi line"""
+s4 = '''This is also
+multi line string'''
+
+# print(s1)
+# print(s2)
+# print(s3)
+# print(s4)
+
+type_of_s4 = type(s4)
+
+# print(type_of_s4)
+
+
+# Int
+n1 = 20
+n2 = -10
+# print(n1)
+# print(n2)
+# print(type(n1))
+
+# Float
+n3 = 2.6
+n4 = -10.0
+
+# print(n3)
+# print(n4)
+# print(type(n4))
+
+# Complex
+
+n5 = 2j
+n6 = -6j
+
+# print(n5)
+# print(n6)
+# print(type(n6))
+
+# Boolean
+b1 = True
+b2 = False
+
+# print(b1)
+# print(b2)
+# print(type(b2))
