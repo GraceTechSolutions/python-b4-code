270671a0fca01f44be8a94022a8dce1af9b6e4e4
JRudransh <JRudransh@protonmail.com>
2022-05-03 15:55:24 +0530

    Clss 2

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 """
 This file can print greetings
 """
-from pprint import pprint
 
 
 greet = 'Hello Python'
@@ -23,6 +22,22 @@ this is multi line"""
 s4 = '''This is also
 multi line string'''
 
+comment = "Jay Said That \"That\'s not my problem\"\n"\
+"\tHello abcdasdfghjkl ffghjkl fghjkl fghjkl dfghj kdfgh jkdfghj fghj fghj"
+# print(comment)
+# \ new line for continuation
+# \\ for displaying \
+# \'                '
+# \"                "
+# \b                backspace
+# \t                tab
+# \n                newline
+
+# print(s1)
+# this is a
+# 012345678
+#        -1
+
 # print(s1)
 # print(s2)
 # print(s3)
@@ -31,6 +46,7 @@ multi line string'''
 type_of_s4 = type(s4)
 
 # print(type_of_s4)
+
 
 
 # Int
@@ -64,3 +80,34 @@ b2 = False
 # print(b1)
 # print(b2)
 # print(type(b2))
+
+# Tuple
+seq1 = (1,)
+seq2 = tuple((1,2,3,4,5,6,7,8,9))
+# print(seq2)
+# print(type(seq2))
+
+# List
+seq3 = [1]
+# print(seq3)
+# print(type(seq3))
+
+# Range
+seq4 = range(0, 31, 3)
+# print(list(seq4))
+# print(type(seq4))
+
+# Set
+col1 = {1,2,3,4,5,6,7,7,8,9,9}
+# print(col1)
+# print(type(col1))
+
+# Dictionary
+student = {
+    'name': 'Jay',
+    'roll_no': 20,
+    'school': 'xy',
+    'grade': 'A+'
+}
+# print(student)
+# print(type(student))
