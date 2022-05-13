213314eb12461be6873b5646fc49032aa1a1ea75
JRudransh <JRudransh@protonmail.com>
2022-05-13 15:45:21 +0530

    Control Statment

---
+++ 2. Operators.py
@@ -0,0 +1,57 @@
+# Arithmatic Operators -> any
+# +
+# -
+# *
+# /
+# %
+# //
+# **
+
+
+# Assignment Operators -> any
+# =
+# +=
+# -=
+# *=
+# /=
+# %=
+# **=
+# //=
+
+# Relational Operators -> bool
+# >
+# >=
+# <
+# <=
+# ==
+# !=
+
+# Logical Operators
+# and
+# or
+# not
+
+# Bitwise Operators
+# ~
+# &
+# |
+# ^
+# <<
+# >>
+
+# 1     0001
+# 2     0010
+# 3     0011
+# 4     0100
+
+# Membership Operators
+
+# in
+# not in
+
+# Identity Operators
+
+# is
+# is not
+
+
---
+++ 3. Control Statment.py
@@ -0,0 +1,48 @@
+# age = 21
+
+# if age >= 18:
+#     print('You can vote')
+# else:
+#     print('You can\'t vote')
+
+# marks = 50
+
+# if marks >= 80:
+#     print('A')
+# elif marks >= 50:
+#     print('B')
+# elif marks >= 40:
+#     print('C')
+# else:
+#     print('Failed')
+
+
+# n = 0
+
+# while n <=10:
+#     print(n)
+#     n+=1
+
+# for i in range(11):
+#     if i == 5:
+#         break
+#     print(i)
+# else:
+#     print('Exited...')
+
+    
+# *
+# * *
+# * * *
+# * * * *
+
+# n = 4
+
+# for i in range(1, n+1):
+#     for j in range(1, i+1):
+#         print('*', end=' ')
+#     print('')
+
+
+# for i in range(1, n+1):
+#     print('* '*i)
